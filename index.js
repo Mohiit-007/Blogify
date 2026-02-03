1f45006d3e1e5c801d1f7ad99be68cef04ad09b7
Mohit Sahu <mohitsahu2005@gmail.com>
2026-02-03 16:56:35 +0530

    commit

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ app.get('/',async (req,res)=>{
 app.use("/user",router);
 app.use("/blog",blogroute);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT,()=>console.log("server started..."))
